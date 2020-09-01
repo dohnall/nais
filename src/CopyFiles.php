968ac42d49a4a0f885244483279207791fa290ed
dohnall <dohnal@gramonet.com>
2020-09-01 12:06:54 +0200

    debugging

--- src/CopyFiles.php
+++ src/CopyFiles.php
@@ -9,7 +9,38 @@ class CopyFiles
 {
     public static function postPackageInstall(PackageEvent $event)
     {
+        echo 'running...<br>';
         $installedPackage = $event->getOperation()->getPackage();
-        echo $installedPackage;
+        $root = dirname(dirname(__FILE__));
+        self::custom_copy($root.'/vendor/laravel/laravel', $root);
+    }
+
+    public static function custom_copy($src, $dst) {
+
+        // open the source directory
+        $dir = opendir($src);
+
+        // Make the destination directory if not exist
+        @mkdir($dst);
+
+        // Loop through the files in source directory
+        while( $file = readdir($dir) ) {
+
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) )
+                {
+
+                    // Recursively calling custom copy function
+                    // for sub directory
+                    self::custom_copy($src . '/' . $file, $dst . '/' . $file);
+
+                }
+                else {
+                    copy($src . '/' . $file, $dst . '/' . $file);
+                }
+            }
+        }
+
+        closedir($dir);
     }
 }
